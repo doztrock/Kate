cce13acbead20acee5438fecf583a010f7ae2084
Ivan Botero <ivan.botero@protonmail.ch>
2017-02-18 12:46:27 -0500

    Added some spanish messages

--- src/lib/language/es.hpp
+++ src/lib/language/es.hpp
@@ -5,7 +5,14 @@
 #ifndef ES_HPP
 #define ES_HPP
 
+#define FILE_NOT_LOADED_SUCCESSFULLY                    "No se pudo cargar archivo"
 
+#define ERROR_VARIABLE_INITIALIZATION(line)             "Error en inicializacion de variable en linea " #line
+#define ERROR_VARIABLE_ASSIGNMENT(line)                 "Error en asignacion de variable en linea " #line
+#define ERROR_VARIABLE_ASSIGNMENT_EMPTY_VALUE(line)     "Error en asignacion de variable, el valor esta vacio en linea " #line
+
+#define ERROR_CONSTANT_INITIALIZATION(line)             "Error con inicializacion de constante en linea " #line
+#define ERROR_CONSTANT_ASSIGNMENT(line)                 "Error! No puedes asignar un valor a una constante previamente inicializada en linea " #line 
 
 #endif /* ES_HPP */
 
