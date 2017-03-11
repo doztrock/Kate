6bb1f17c63e9bc8de96e214c0b97c8675ffc8e11
Ivan Botero <ivan.botero@protonmail.ch>
2017-03-11 10:16:02 -0500

    Changes in function.h

--- src/lib/function.hpp
+++ src/lib/function.hpp
@@ -9,6 +9,38 @@
 #include "memory.hpp"
 #include "library.hpp"
 
+
 using namespace std;
+
+
+/**
+ * 
+ * FUNCTION STRUCTURE
+ * 
+ */
+typedef struct __function_structure {
+    const string NAME;
+    unsigned int ARGC;
+} Function;
+
+
+/**
+ * 
+ * FUNCTION CONTAINER
+ * 
+ */
+static const int FUNCTION_CONTAINER_LENGTH = 3;
+
+static const Function FUNCTION_CONTAINER[FUNCTION_CONTAINER_LENGTH] = {
+
+    /* Printing Functions */
+    {"print", 1},
+    {"println", 1},
+
+    /* Exit Function */
+    {"exit", 0}
+
+};
+
 
 #endif /* FUNCTION_HPP */
